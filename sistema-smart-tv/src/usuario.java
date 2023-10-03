5d5db0ac9ef62f8d3646878a74406df406e0f6e5
lucinoro1 <luciano.rodriguesoliveira1@gmail.com>
2023-10-03 20:25:28 -0300

    commit smarttv

---
+++ sistema-smart-tv/src/usuario.java
@@ -0,0 +1,22 @@
+public class usuario {
+    public static void main(String[] args) throws Exception {
+    
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume Atual :" +smartTv.volume);
+
+        System.out.println("TV Ligada ? " + smartTv.ligada);
+        System.out.println("Canal Atual ? " + smartTv.canal);
+        System.err.println("Volume Atual ? " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo status -> TV Ligada ? " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("Novo status -> TV Ligada ? " + smartTv.ligada);
+    }
+}
